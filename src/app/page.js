c5739c0e7459a402751e494a87006753e3b94f96
congminh-git <didotekdevs@gmail.com>
2024-01-26 18:52:33 +0700

    port-card first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,24 @@
 //Home Page
 
+import PostCard from '@/components/client-user/post-card/port-card';
 import ScrollButton from '@/components/client-user/scroll-button/scroll-button';
 
 export default function Home() {
     return (
-        <div className="sm:mt-20 mt-16">
-            <div className="h-screen bg-slate-200"></div>
-            <div className="h-screen bg-green-200"></div>
-            <div className="h-screen bg-red-200"></div>
-            <div className="h-screen bg-gray-200"></div>
-            <div className="h-screen bg-blue-200"></div>
-            <div className="h-screen bg-pink-200"></div>
-            <div className="h-screen bg-orange-200"></div>
+        <div className=" flex justify-center">
+            <div className="sm:mt-20 mt-16 w-full xl:max-w-screen-xl">
+                <div>
+                    <h2>Bài đăng gần đây</h2>
+                    <div className="list-posts w-full p-2">
+                        <PostCard />
+                        <PostCard />
+                        <PostCard />
+                        <PostCard />
+                        <PostCard />
+                        <PostCard />
+                    </div>
+                </div>
+            </div>
             <ScrollButton />
         </div>
     );
